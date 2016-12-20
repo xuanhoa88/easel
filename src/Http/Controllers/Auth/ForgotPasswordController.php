5854479770779a9fb40b232eb3ca5a8fc21c2d57
talvbansal <talvbansal@hotmail.com>
2016-12-20 12:11:53 +0000

    Remove functional tests, as per #14 they belong in the main canvas repo

--- src/Http/Controllers/Auth/ForgotPasswordController.php
+++ src/Http/Controllers/Auth/ForgotPasswordController.php
@@ -34,4 +34,14 @@ class ForgotPasswordController extends Controller
     {
         $this->middleware('guest');
     }
+
+    /**
+     * Display the form to request a password reset link.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function showLinkRequestForm()
+    {
+        return view('canvas::auth.passwords.email');
+    }
 }
