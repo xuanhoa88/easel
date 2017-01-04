ec9289c78c53a625e0588d9145e5dcb671730582
Todd Austin <austin.todd.j@gmail.com>
2017-01-04 07:25:57 -0600

    Removed help modal

--- resources/views/backend/profile/partials/form/social-networks.blade.php
+++ resources/views/backend/profile/partials/form/social-networks.blade.php
@@ -38,7 +38,7 @@
 
 <div class="form-group">
     <div class="fg-line">
-        <label class="fg-label">Resume/CV <a href="" data-toggle="modal" data-target="#resume-help"><i class="zmdi zmdi-help" data-toggle="tooltip" data-placement="bottom" title="" data-original-title="Resume/CV Help"></i></a></label>
+        <label class="fg-label">Resume/CV</label>
         <div class="input-group">
             <input type="text" class="form-control" name="resume_cv" id="resume_cv" value="{{ $data['resume_cv'] }}" placeholder="Example: my_resume.pdf" v-model="resume_cv">
             <span class="input-group-btn" style="margin-bottom: 11px">
