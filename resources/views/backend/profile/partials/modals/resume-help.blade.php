788a27a05e355f997e1c4060cdb4242bf61b1ceb
Todd Austin <austin.todd.j@gmail.com>
2017-01-03 22:19:03 -0600

    Profile modal update

--- resources/views/backend/profile/partials/modals/resume-help.blade.php
+++ resources/views/backend/profile/partials/modals/resume-help.blade.php
@@ -6,8 +6,8 @@
                 <h4 class="modal-title">Resume/CV Help</h4>
             </div>
             <div class="modal-body">
-                <p>Allowing users to download your Resume/CV is simple, just drop in the <code>.pdf</code> path relative to <code>public/uploads/</code>.</p>
-                <p><strong>For example</strong>, if you'd like to use the <em>my_resume</em> document, just save <code>my_resume.pdf</code> in the input.</p>
+                <p>Allowing users to download your Resume/CV is simple, just drop in the <code>.pdf</code> path relative to the <code>storage</code> directory.</p>
+                <p><strong>For example</strong>, if you'd like to use a document you've uploaded called <em>my_resume</em>, just save <code>my_resume.pdf</code> in this field.</p>
             </div>
             <div class="modal-footer">
                 <a href="" class="btn btn-link" data-dismiss="modal">Close</a>
