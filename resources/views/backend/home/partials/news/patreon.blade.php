bbf1535c612b37dcd4d1b98f942125b0a4975fd4
Todd Austin <austin.todd.j@gmail.com>
2017-01-23 14:07:53 -0600

    :newspaper: Added Patreon news story

--- resources/views/backend/home/partials/news/patreon.blade.php
+++ resources/views/backend/home/partials/news/patreon.blade.php
@@ -11,11 +11,12 @@
 <div id="collapseFour" class="collapse" role="tabpanel"
      aria-labelledby="headingFour">
     <div class="panel-body">
+        <a href="https://www.patreon.com/canvas" target="_blank"><img src="{!! asset('vendor/canvas/assets/images/news/patreon.png') !!}" class="img-responsive"></a><br>
         <a href="https://www.patreon.com/canvas" target="_blank"><strong>Canvas is officially on Patreon!</strong></a> If you've been using
         Canvas for some time now and want to show your love and support for the platform, consider
         <a href="https://www.patreon.com/bePatron?u=4872869" target="_blank">making a pledge</a> to
         keep the bug fixes, issue resolution, and app updates progressing at a rapid pace. Pledges start as low as
-        $2, and some pretty cool rewards accompany each level. Maintaining and developing new
+        $2, and some pretty cool rewards accompany each level. <br><br>Maintaining and developing new
         features for Canvas takes a considerable amount of time. Regardless of what is generated here, I'll
         continue to develop this project because it's a passion of mine, <em>but a cup of coffee every once in a while definitely helps!</em>
     </div>
