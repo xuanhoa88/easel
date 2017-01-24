e6066bafbd911336d0bbc922a1949e00ba14c9f1
Todd Austin <austin.todd.j@gmail.com>
2017-01-24 13:21:31 -0600

    Updates site footer links for support

--- resources/views/backend/partials/footer.blade.php
+++ resources/views/backend/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <footer id="footer">
-    Thank you for creating with <a href="http://canvas.toddaustin.io" target="_blank">Canvas</a>&nbsp;&#183;&nbsp; {{ Canvas\Models\Settings::canvasVersion() }}
+    Thank you for creating with <a href="https://canvas.toddaustin.io" target="_blank">Canvas</a>&nbsp;&#183;&nbsp; {{ Canvas\Models\Settings::canvasVersion() }}
 
     <ul class="f-menu">
         <li><a href="{!! route('canvas.admin') !!}">Home</a></li>
@@ -8,6 +8,6 @@
         <li><a href="{!! route('canvas.admin.upload') !!}">Media</a></li>
         <li><a href="{!! route('canvas.admin.tools') !!}">Tools</a></li>
         <li><a href="{!! route('canvas.admin.settings') !!}">Settings</a></li>
-        <li><a href="mailto:austin.todd.j@gmail.com">Support</a></li>
+        <li><a href="https://cnvs.readme.io/discuss" target="_blank">Support</a></li>
     </ul>
 </footer>
