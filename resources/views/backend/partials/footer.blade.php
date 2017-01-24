720ecddab808fd4b60249304d71da28678c68919
Todd Austin <austin.todd.j@gmail.com>
2017-01-24 14:33:01 -0600

    Cleaned up footer links

--- resources/views/backend/partials/footer.blade.php
+++ resources/views/backend/partials/footer.blade.php
@@ -1,13 +1,11 @@
 <footer id="footer">
-    Thank you for creating with <a href="https://canvas.toddaustin.io" target="_blank">Canvas</a>&nbsp;&#183;&nbsp; {{ Canvas\Models\Settings::canvasVersion() }}
+    Thank you for creating with <a href="https://canvas.toddaustin.io" target="_blank">Canvas</a>&nbsp;&#183;&nbsp;{{ Canvas\Models\Settings::canvasVersion() }}
 
     <ul class="f-menu">
         <li><a href="{!! route('canvas.admin') !!}">Home</a></li>
         <li><a href="{!! route('canvas.admin.post.index') !!}">Posts</a></li>
         <li><a href="{!! route('canvas.admin.tag.index') !!}">Tags</a></li>
         <li><a href="{!! route('canvas.admin.upload') !!}">Media</a></li>
-        <li><a href="{!! route('canvas.admin.tools') !!}">Tools</a></li>
-        <li><a href="{!! route('canvas.admin.settings') !!}">Settings</a></li>
-        <li><a href="https://cnvs.readme.io/discuss" target="_blank">Support</a></li>
+        <li><a href="{!! route('canvas.admin.help') !!}">Help</a></li>
     </ul>
 </footer>
