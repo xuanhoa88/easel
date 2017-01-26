19678693a8cb84019592b110a6033f68342f79ec
Todd Austin <austin.todd.j@gmail.com>
2017-01-26 14:12:08 -0600

    :link: Footer links refresh

--- resources/views/backend/partials/footer.blade.php
+++ resources/views/backend/partials/footer.blade.php
@@ -2,10 +2,10 @@
     Thank you for creating with <a href="https://canvas.toddaustin.io" target="_blank">Canvas</a>&nbsp;&#183;&nbsp;{{ Canvas\Models\Settings::canvasVersion() }}
 
     <ul class="f-menu">
-        <li><a href="{!! route('canvas.admin') !!}">Home</a></li>
-        <li><a href="{!! route('canvas.admin.post.index') !!}">Posts</a></li>
-        <li><a href="{!! route('canvas.admin.tag.index') !!}">Tags</a></li>
-        <li><a href="{!! route('canvas.admin.upload') !!}">Media</a></li>
-        <li><a href="{!! route('canvas.admin.help') !!}">Help</a></li>
+        <li><a href="https://cnvs.readme.io/docs" target="_blank">Documentation</a></li>
+        <li><a href="https://cnvs.readme.io/blog" target="_blank">Blog</a></li>
+        <li><a href="https://github.com/cnvs/canvas" target="_blank">GitHub</a></li>
+        <li><a href="https://cnvs.hipchat.com" target="_blank">HipChat</a></li>
+        <li><a href="https://cnvs.readme.io/discuss" target="_blank">Support</a></li>
     </ul>
 </footer>
