bc698ae723b158b22df2bb3417895a096cb66ccf
Todd Austin <austin.todd.j@gmail.com>
2017-01-31 14:16:04 -0600

    :recycle: Revert ensure installed command tweak

--- src/Http/Middleware/EnsureInstalled.php
+++ src/Http/Middleware/EnsureInstalled.php
@@ -9,9 +9,6 @@ class EnsureInstalled
 {
     /**
      * Handle an incoming request.
-     * Since TravisCI does not allow for human interaction from
-     * the CLI for canvas:install, we just make a hard redirect
-     * to skip the installer.
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
@@ -19,14 +16,10 @@ class EnsureInstalled
      */
     public function handle($request, Closure $next)
     {
-        switch (env('APP_ENV') === 'testing') {
-            case true:
-                return redirect()->route('canvas.home');
-            case false:
-                return redirect()->route('canvas.install');
-            default:
-                return $next($request);
-                break;
+        if (! SetupHelper::isInstalled()) {
+            return redirect()->route('canvas.install');
         }
+
+        return $next($request);
     }
 }
