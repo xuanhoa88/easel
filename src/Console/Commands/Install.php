c7965986edc731b211b6347a4455b4541497de25
Todd Austin <austintoddj@users.noreply.github.com>
2017-02-08 09:58:17 -0600

    Apply fixes from StyleCI (#81)

--- src/Console/Commands/Install.php
+++ src/Console/Commands/Install.php
@@ -43,7 +43,7 @@ class Install extends CanvasCommand
     {
         if (file_exists(storage_path('canvas_installed.lock'))) {
             $date = file_get_contents(storage_path('canvas_installed.lock'));
-            $this->line('Installed on ' . date('F j, Y', $date));
+            $this->line('Installed on '.date('F j, Y', $date));
             $this->comment('Canvas has already been installed on this system.');
         } else {
             $config = ConfigHelper::getWriter();
