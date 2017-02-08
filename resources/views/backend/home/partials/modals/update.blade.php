fb9e6afb4d8ada3d35158c19461ef69827ba15ad
Todd Austin <austin.todd.j@gmail.com>
2017-02-08 08:13:50 -0600

    Update modal now includes SemVer specifics

--- resources/views/backend/home/partials/modals/update.blade.php
+++ resources/views/backend/home/partials/modals/update.blade.php
@@ -6,8 +6,16 @@
                 <h4 class="modal-title">Updating Canvas</h4>
             </div>
             <div class="modal-body">
-                <p>To upgrade your installation of Canvas to the latest available, just run the following command from a terminal:</p>
-                <p><code>php artisan canvas:update</code></p>
+                <h4>Minor/Patch Version Upgrade</h4>
+                <p><small><em>Example: Upgrading from v3.1.0 to v3.1.1</em></small></p>
+                <p>Run <code>php artisan canvas:update</code> from a terminal to upgrade your installation of Canvas to the latest version.</p>
+
+                <h4>Major Version Upgrade</h4>
+                <p><small><em>Example: Upgrading from v3.0.5 to v3.1.0</em></small></p>
+                <p>We attempt to document every possible breaking change to ensure a smooth transition between versions. To begin a major version upgrade, visit the <a href="https://cnvs.readme.io/docs/upgrade-guide" target="_blank">Official Upgrade Guide</a> and follow the steps.</p>
+
+                <h4>More Information</h4>
+                <p>Find out more information about <a href="http://semver.org" target="_blank">Semantic Versioning</a>. Canvas follows SemVer 2.0.0 in its software release cycle.</p>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-link" data-dismiss="modal">Close</button>
