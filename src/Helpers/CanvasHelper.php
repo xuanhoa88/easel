972717d6209dead8efb385af606a9cca87e2a7a6
Todd Austin <austin.todd.j@gmail.com>
2017-02-23 13:00:58 -0600

    Code cleanup and minor updates

--- src/Helpers/CanvasHelper.php
+++ src/Helpers/CanvasHelper.php
@@ -100,7 +100,7 @@ class CanvasHelper extends Constants
         $packageName = self::CORE_PACKAGE;
         $version = 'Unknown';
 
-        // Retrieve core (Easel) package info.
+        // Retrieve framework (Easel) package info.
         $core = $extMan->getExtension(str_replace('/', '-', self::CORE_PACKAGE), ['canvas-framework']);
         $version = $core->getVersion();
         $dist = $core->__get('dist');
