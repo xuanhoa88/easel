e5874238dc3410a713767b4c142647781065736d
Todd Austin <austin.todd.j@gmail.com>
2017-02-23 07:51:34 -0600

    :beer: Gulp production

--- resources/views/frontend/blog/partials/author.blade.php
+++ resources/views/frontend/blog/partials/author.blade.php
@@ -1,20 +1,20 @@
-<img style="float: left; width: 60px" class="img-responsive img-circle author-img" src="https://www.gravatar.com/avatar/{{ md5($user->email) }}?d=identicon&s=150" title="{{ $user->first_name .  ' ' . $user->last_name }}">
-<div style="overflow: hidden">
+<img class="img-responsive img-circle author-img" src="https://www.gravatar.com/avatar/{{ md5($user->email) }}?d=identicon&s=150" title="{{ $user->first_name .  ' ' . $user->last_name }}">
+<div id="author-content">
     <h4 id="auth-name"><strong>{{ $user->first_name .  ' ' . $user->last_name }}</strong></h4>
-    <span class="small" style="margin-top: 0">
+    <span class="small">
         {{ $user->bio }}
         <br>
         @if (!empty($user->twitter))
-            <a href="https://twitter.com/{{ $user->twitter }}" target="_blank" id="social"><i class="fa fa-fw fa-twitter text-muted" style="font-size: 14px"></i></a>
+            <a href="https://twitter.com/{{ $user->twitter }}" target="_blank" id="social"><i class="fa fa-fw fa-twitter text-muted"></i></a>
         @endif
         @if (!empty($user->facebook))
-            <a href="https://facebook.com/{{ $user->facebook }}" target="_blank" id="social"><i class="fa fa-fw fa-facebook text-muted" style="font-size: 14px"></i></a>
+            <a href="https://facebook.com/{{ $user->facebook }}" target="_blank" id="social"><i class="fa fa-fw fa-facebook text-muted"></i></a>
         @endif
         @if (!empty($user->github))
-            <a href="https://github.com/{{ $user->github }}" target="_blank" id="social"><i class="fa fa-fw fa-github text-muted" style="font-size: 14px"></i></a>
+            <a href="https://github.com/{{ $user->github }}" target="_blank" id="social"><i class="fa fa-fw fa-github text-muted"></i></a>
         @endif
         @if(!empty($user->linkedin))
-            <a href="https://linkedin.com/in/{{ $user->linkedin }}" target="_blank" id="social"><i class="fa fa-fw fa-linkedin text-muted" style="font-size: 14px"></i></a>
+            <a href="https://linkedin.com/in/{{ $user->linkedin }}" target="_blank" id="social"><i class="fa fa-fw fa-linkedin text-muted"></i></a>
         @endif
     </span>
 </div>
