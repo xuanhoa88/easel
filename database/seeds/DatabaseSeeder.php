6e63938cd39b1b9f47276488abbe6b1d8ae0a356
Todd Austin <austin.todd.j@gmail.com>
2017-03-07 10:16:35 -0600

    Updates DatabaseSeeder per https://github.com/cnvs/easel/commit/dc8a46dd5b211cd58b6921c5309194eb77032d42#commitcomment-21186409

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call('Canvas\PostTableSeeder');
-        $this->call('Canvas\TagTableSeeder');
-        $this->call('Canvas\PostTagTableSeeder');
+        $this->call(PostTableSeeder::class);
+        $this->call(TagTableSeeder::class);
+        $this->call(PostTagTableSeeder::class);
     }
 }
